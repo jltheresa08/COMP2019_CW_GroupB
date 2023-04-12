7b40cb9aaa321281afa6191fc705a026b2433a2b
jltheresa08 <65333063+jltheresa08@users.noreply.github.com>
2023-04-12 17:09:59 +0800

    Delete SubmitBtn.jsx

--- src/components/SubmitBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SubmitBtn = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Submit
-  </button>
-);
-
-export default SubmitBtn;
